302a79c3216777f629324dd2756e8d77dfdc6487
Kayleighs <47003302+kayleighs@users.noreply.github.com>
2019-05-10 17:00:48 -0400

    started html and js

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+var gemTotal = 0;
+var totalGemNeeded;
+var wins = 0;
+var losses = 0;
+
+$(document).ready(function(){
+
+totalGemNeeded = Math.floor((Math.random() * 120) + 19)
+
+emerald = Math.floor((Math.random() * 12) + 1)
+diamond = Math.floor((Math.random() * 12) + 1)
+ruby = Math.floor((Math.random() * 12) + 1)
+sapphire = Math.floor((Math.random() * 12) + 1)
+
+
+
+
+
+
+
+
+});
